58696650b4d713b38cb5b9c70a3d424eef907141
KristiyanBogdanov <71849513+KristiyanBogdanov@users.noreply.github.com>
2020-10-23 17:09:09 +0300

    [UPDATE] - Final version with working ex. 3

--- Classwork/Classwork-23.10.20.py
+++ Classwork/Classwork-23.10.20.py
@@ -1,53 +1,69 @@
-# не работи задачата с простите числа
+def run_exercise_one():
+    print("First exercise:")
 
-def is_prime(n):
-	if n <= 1 or n % 1 > 0:
-		return False
-	for i in range(2, n//2):
-		if n % 1 == 0:
-			return False
-	return True
+    result = 0
+    number = int(input("Enter your number: "))
+
+    for num in range(number + 1):
+        result += num
+
+    print(f"Result: {result}")
 
 
-result = 0
-for x in range(int(input()) + 1):
-	result += x
-	
-print(result)
+def run_exercise_two():
+    print("\nSecond exercise:")
 
-ll = [1, 5, 4, 16, 0, 52, 17]
-odd_numbers = 0
-even_numbers = 0
+    numbers = [1, 5, 4, 16, 0, 52, 17]
+    odd_numbers_count = 0
+    even_numbers_count = 0
 
-for number in ll:
-	if number != 0:	
-		if number % 2 == 0:
-			odd_numbers += 1
-		else:
-			even_numbers += 1
-		
-print(f"Odd count: {odd_numbers}, Even count: {even_numbers}")
+    for number in numbers:
+        if number != 0:
+            if number % 2:
+                even_numbers_count += 1
+            else:
+                odd_numbers_count += 1
 
-age = int(input())
-person_age = 0.0
+    print(f"Odd numbers (count): {odd_numbers_count},"
+          f" Even numbers (count): {even_numbers_count}")
 
-for x in range(1, age + 1):
-	person_age += 10.5 if x <= 2 else 4.0
-		
-print(person_age)
 
-n = int(input())
-number = 2
-prime_number = []
+def run_exercise_three():
+    print("\nThird exercise:")
 
-while True:
-	if number == n:
-		break
-	if is_prime(number):
-		prime_number.append(number)
-	else:
-		number += 1
-		
-print(prime_number)
-	
-	
+    n = int(input("Enter your number: ")) - 1
+    prime_numbers = [2]
+    current_number = 2
+
+    while n:
+        if is_prime(current_number):
+            prime_numbers.append(current_number)
+            n -= 1
+        current_number += 1
+
+    print(f"Prime numbers to n: {', '.join(map(str, prime_numbers))}")
+
+
+def is_prime(number):
+    for num in range(2, number):
+        return False if number % num == 0 else True
+
+
+def run_exercise_four():
+    print("\nForth exercise:")
+
+    age = int(input("Enter dog age: "))
+    person_age = 0.0
+
+    for x in range(1, age + 1):
+        person_age += 10.5 if x <= 2 else 4.0
+
+    print(f"Dog age - {age} || Person age - {int(person_age)}")
+
+
+print("Classwork - 23.10.2020\n")
+
+run_exercise_one()
+run_exercise_two()
+run_exercise_three()
+run_exercise_four()
